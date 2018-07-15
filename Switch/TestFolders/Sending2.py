a145e9191b956176f0a44d45fa606a47e8fffca3
lilDarwin <39616823+lilDarwin@users.noreply.github.com>
2018-07-15 13:02:28 +0200

    final change
    Finalchange to the sending, this format of sending is the most appropriate for switching thereforeshould be kept as a reference.

--- Switch/TestFolders/Sending2.py
+++ Switch/TestFolders/Sending2.py
@@ -5,10 +5,13 @@ import struct
 import time
 from time import sleep
 
-#ser = serial.Serial(port='COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, write_timeout = 1)
-ser = serial.Serial(port='/dev/cu.usbmodem1411', baudrate=9600, bytesize=8, parity='N', stopbits=1, write_timeout = 1, dsrdtr = True)
-
+ser = serial.Serial(port='COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, write_timeout = 1, dsrdtr = True)
+#ser = serial.Serial(port='/dev/cu.usbmodem1411', baudrate=9600, bytesize=8, parity='N', stopbits=1, write_timeout = 1, dsrdtr = True)
+sendlist=[]
 testlist = [0, 0, 0 ,0, 0, 0, 0, 0]
+for i in range(len(testlist)):
+	sendlist.append(str(testlist[i]))
+
 ser.write("<".encode())
 print("Sending <")
 
@@ -16,8 +19,10 @@ time.sleep(0.5)
 
 x = 100
 
-ser.write("255, 1, 7, 6, 30, 254, 123, 900".encode())
-
+#ser.write("0, 0, 0, 0, 32, 32, 0, 0".encode())
+ser.write(sendlist[0].encode()+ ",".encode() +sendlist[1].encode()+ ",".encode() +sendlist[2].encode()+ 
+	",".encode() +sendlist[3].encode()+ ",".encode() +sendlist[4].encode()+ ",".encode() +
+	sendlist[5].encode()+ ",".encode() +sendlist[6].encode()+ ",".encode() +sendlist[7].encode())
 
 print("Sending")
 
